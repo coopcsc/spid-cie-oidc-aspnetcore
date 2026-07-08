a18231c8a74a8839116e6d326edf7533f146e043
tempcode <piemp@hotmail.it>
2026-07-08 15:17:37 +0200

    fix: starvation on retrieve idp selector

--- src/Spid.Cie.OIDC.AspNetCore/Configuration/OpenIdConnectOptionsProvider.cs
+++ src/Spid.Cie.OIDC.AspNetCore/Configuration/OpenIdConnectOptionsProvider.cs
@@ -2,12 +2,10 @@
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Protocols;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Spid.Cie.OIDC.AspNetCore.Services;
 using System;
 using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace Spid.Cie.OIDC.AspNetCore.Configuration;
 
@@ -15,21 +13,18 @@ class OpenIdConnectOptionsProvider : IOptionsMonitor<OpenIdConnectOptions>
 {
     readonly ConcurrentDictionary<string, Lazy<OpenIdConnectOptions>> _cache;
     readonly IOptionsFactory<OpenIdConnectOptions> _optionsFactory;
-    readonly IIdentityProviderSelector _idpSelector;
     readonly IConfigurationManager<OpenIdConnectConfiguration> _configurationManager;
     readonly CustomHttpClientHandler _httpClientHandler;
     readonly IHttpClientFactory _httpClientFactory;
 
     public OpenIdConnectOptionsProvider(
         IOptionsFactory<OpenIdConnectOptions> optionsFactory,
-        IIdentityProviderSelector idpSelector,
         IConfigurationManager<OpenIdConnectConfiguration> configurationManager,
         CustomHttpClientHandler httpClientHandler,
         IHttpClientFactory httpClientFactory)
     {
         _cache = new ConcurrentDictionary<string, Lazy<OpenIdConnectOptions>>();
         _optionsFactory = optionsFactory;
-        _idpSelector = idpSelector;
         _configurationManager = configurationManager;
         _httpClientHandler = httpClientHandler;
         _httpClientFactory = httpClientFactory;
@@ -38,9 +33,11 @@ class OpenIdConnectOptionsProvider : IOptionsMonitor<OpenIdConnectOptions>
     [ExcludeFromCodeCoverage]
     public OpenIdConnectOptions CurrentValue => Get(Options.DefaultName);
 
-    public OpenIdConnectOptions Get(string name)
+    public OpenIdConnectOptions Get(string? name)
     {
-        var provider = Task.Run(async () => await _idpSelector.GetSelectedIdentityProvider()).Result;
+        if (string.IsNullOrWhiteSpace(name))
+            name = Options.DefaultName;
+            
         var options = _cache.GetOrAdd(name, _ => new Lazy<OpenIdConnectOptions>(() => _optionsFactory.Create(name))).Value;
         if (name.Equals(SpidCieConst.AuthenticationScheme))
         {
